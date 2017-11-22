af2b2074f792c7f4e3ab70c8812d4cb6194ee701
joemcmanus <joe.mcmanus@colorado.edu>
2017-11-22 00:03:43 -0700

    Beta release ready

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # packetexaminer
-Python PCAP Examiner
+This is a somple harness to do PCAP analysis. This hopefully automates a lot of the function an analyst would do manually. 
+
+It supports:
+-Counts of IPs
+-Bytes between IPS
+-DNS lookups
+-URLs
+
--- packetexaminer.py
+++ packetexaminer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# File    : packetex.py
+# File    : packetexaminer.py
 # Author  : Joe McManus josephmc@alumni.cmu.edu
 # Version : 0.1  11/21/2017 Joe McManus
 # Copyright (C) 2017 Joe McManus
@@ -44,6 +44,8 @@ parser.add_argument('--flows', help="Display flow summary", action="store_true")
 parser.add_argument('--dst', help="Display count of destination IPs", action="store_true")
 parser.add_argument('--src', help="Display count of source IPs", action="store_true")
 parser.add_argument('--bytes', help="Display source and destination byte counts", action="store_true")
+parser.add_argument('--dns', help="Display all DNS Lookups in PCAP", action="store_true")
+parser.add_argument('--url', help="Display all ULRs in PCAP", action="store_true")
 parser.add_argument('--all', help="Display all", action="store_true")
 parser.add_argument('--limit', help="Limit results to X", type=int)
 args=parser.parse_args()
@@ -54,6 +56,21 @@ if args.all:
     args.bytes=True
     args.dst=True
     args.src=True
+    args.dns=True
+    args.url=True
+
+if args.url:
+    try:
+        from scapy_http import http
+    except:
+        print("""ERROR: Scapy does not have http support, skipping url mining.
+        You can try the following: 
+            wget https://github.com/invernizzi/scapy-http/archive/master.zip
+            unzip master.zip
+            cd scapy-http-master
+            sudo python3 ./setup.py build install. """)
+        args.url=False
+
 
 table= PrettyTable(["Option", "Value"])
 table.add_row(["File", args.file])
@@ -62,6 +79,8 @@ table.add_row(["Bytes", args.bytes])
 table.add_row(["Flows", args.flows])
 table.add_row(["Dst", args.dst])
 table.add_row(["Src", args.src])
+table.add_row(["DNS", args.dns])
+table.add_row(["URLs", args.url])
 print(table)
 
 if os.path.isfile(args.file):
@@ -81,9 +100,6 @@ for pkt in pkts:
         srcIP.append(pkt[IP].src)
         dstIP.append(pkt[IP].dst)
         srcdst.append(pkt[IP].src + ","  + pkt[IP].dst)
-        i+=1
-        #if i % 1000 == 0 :
-        #    print("Processed {} packets".format(i))
 
 def simpleCount(ipList, limit, headerOne, headerTwo, title):
     table= PrettyTable([headerOne, headerTwo])
@@ -139,6 +155,29 @@ def byteCount(pkts, srcdst, limit):
 
     print(table)
 
+def dnsCount(pkts, limit, headerOne, headerTwo, title):
+    lookups=[]
+    for pkt in pkts:
+        if IP in pkt:
+            if pkt.haslayer(DNS) and pkt.getlayer(DNS).qr == 0:
+                lookup=(pkt.getlayer(DNS).qd.qname).decode("utf-8")
+                if "arpa" not in lookup:
+                    lookups.append(lookup)
+     
+    simpleCount(lookups, limit, headerOne, headerTwo, title)
+
+def urlCount(pkts, limit, headerOne, headerTwo, title):
+    urls=[]
+    for pkt in pkts:
+        if IP in pkt:
+            if http.HTTPRequest in pkt:
+                uri=(pkt[http.HTTPRequest].Path).decode("utf-8")
+                host=(pkt[http.HTTPRequest].Host).decode("utf-8")
+                urls.append(host+uri)
+                print(host+uri)
+    simpleCount(urls, limit, headerOne, headerTwo, title)
+
+
 
 if args.src:
     simpleCount(srcIP, args.limit, "Source IP", "Count", "Source IP Occurence")
@@ -148,7 +187,7 @@ if args.flows:
     flowCount(srcdst, args.limit)
 if args.bytes:
     byteCount(pkts, srcdst, args.limit)
-
-#Top DNS 
-#Top HTTP 
-#Top Ports
+if args.dns:
+    dnsCount(pkts, args.limit, "DNS Lookup", "Count", "Unique DNS Lookups")
+if args.url:
+    urlCount(pkts, args.limit, "URL", "Count", "Unique URLs" )
